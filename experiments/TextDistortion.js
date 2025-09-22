2c8aa0cef245deb4b457e5b6f1be9dc0c79c515a
Denisa-Georgiana <151851868+Denisa-Georgiana-23@users.noreply.github.com>
2025-09-22 22:21:18 +0200

    TextDistortion.js
    Started on a new experiment about text distortion and movement

---
+++ experiments/TextDistortion.js
@@ -0,0 +1,14 @@
+/*
+----- Coding Tutorial by Patt Vira ----- 
+Name: Text Distortion
+Video Tutorial: https://youtu.be/9mucfJmwupk
+----------------------------------------
+*/
+
+function setup() {
+    createCanvas (2068, 940);
+}
+
+function draw() {
+    background(220);
+}
