8779d472ee67688beea628a03aabc4d9ea7514c2
Denisa-Georgiana <151851868+Denisa-Georgiana-23@users.noreply.github.com>
2025-09-22 22:35:34 +0200

    Update text distortion
    created the circle with 30 points

--- experiments/TextDistortion.js
+++ experiments/TextDistortion.js
@@ -5,10 +5,24 @@ Video Tutorial: https://youtu.be/9mucfJmwupk
 ----------------------------------------
 */
 
+let ptsPerRing = 30;
+let r = 100;
+let shapeSize = 5;
+
 function setup() {
     createCanvas (2068, 940);
 }
 
 function draw() {
     background(220);
+    noStroke();
+    fill(0);
+    //translating the origin of the cirle
+    translate(width/2, height/2);
+    for (let i=0; i<ptsPerRing; i++) {
+        let angle = TWO_PI/ptsPerRing * i;
+        let x = r*cos(angle);
+        let y = r*sin(angle);
+        ellipse(x, y, shapeSize);
+    }
 }
