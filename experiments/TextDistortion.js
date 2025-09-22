036a0dee0e2429f57fb658298ef71fe3941fa10f
Denisa-Georgiana <151851868+Denisa-Georgiana-23@users.noreply.github.com>
2025-09-23 01:18:51 +0200

    repeated text
    made so that the text looks better and added the option of repeating the text multiple times horizontally and vertically

--- experiments/TextDistortion.js
+++ experiments/TextDistortion.js
@@ -6,8 +6,8 @@ Video Tutorial: https://youtu.be/9mucfJmwupk
 */
 
 let ptsPerRing = 200;
-let minR = 90; let maxR = 500;
-let numRings = 100;
+let minR = 50; let maxR = 1100;
+let numRings = 170;
 let shapeSize = 5;
 let distortionFactor = 0.1;
 let freqFactor = 0.02;
@@ -37,9 +37,16 @@ function draw() {
             let angle = TWO_PI/ptsPerRing * j;
           
             let distortedAngle = angle + ampFactor*sin(freqFactor*r);
+            
+            //modulo horizontal repeat text horizontal
+            let repeatX = 1; 
+            let sampleX = (angle * repeatX) % TWO_PI; 
+
             //image mapping points
-            let refX = int(map(angle, 0, TWO_PI, 0, img.width));
-            let sampleY = (r-minR) / (maxR - minR);
+            let refX = int(map(sampleX, 0, TWO_PI, 0, img.width));
+            //modulo!!! check name again makes it so that text repeats vertically
+            let repeatY = 3; 
+            let sampleY = ((r-minR) / (maxR - minR) * repeatY) % 1.0;
             let refY = int(map(sampleY, 0, 1, 0, img.height));
             let c = img.get(refX, refY);
 
