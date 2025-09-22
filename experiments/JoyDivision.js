3b6f3e2dc367b3e5fe139a0b7c95f447e0b38acd
Denisa-Georgiana <151851868+Denisa-Georgiana-23@users.noreply.github.com>
2025-09-22 20:25:34 +0200

    finsihed Joy Division

--- experiments/JoyDivision.js
+++ experiments/JoyDivision.js
@@ -17,5 +17,32 @@ function setup() {
 
 function draw() {
     background(0);
-    let offset = 0; 
+    let offset = 0;
+
+    //lines visual
+    stroke(255);
+    strokeWeight(0.8);
+    fill(0);
+
+    for (let i=0; i<num; i++) {
+        beginShape();
+        for (let j=marginX; j<width - marginX; j++) {
+            let distanceFromCenter = abs(j - width/2);
+
+            let radius;
+            let r = width/2 - sidesValue - distanceFromCenter;
+
+            if (r >= 0) {
+                radius = r / 2;
+            } else {
+                radius = 0;
+            }
+
+            let n = noise(offset);
+            let y = marginY + i*spacing + (n * radius * -1);
+            vertex(j, y);
+            offset += inc;
+        }
+        endShape();
+    }
 }
